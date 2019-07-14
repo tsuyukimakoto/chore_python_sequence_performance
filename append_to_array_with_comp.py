0a5db7e4c9607aba5224c9aa3d1c3a51996925f1
makoto tsuyuki <mtsuyuki@gmail.com>
2019-07-14 14:51:53 +0900

    add array

---
+++ append_to_array_with_comp.py
@@ -0,0 +1,22 @@
+from array import array
+from random import randint
+import sys
+
+
+def create_data():
+    return array('i', [i for i in range(100000)])
+
+
+def proc():
+    cnt = 0
+    data = create_data()
+    for i in range(100000):
+        if randint(1, 10000000) in data:
+            cnt += 1
+
+if __name__ == '__main__':
+    print(sys.argv[0])
+    print(sys.version_info)
+    import timeit
+    print(timeit.timeit("proc()", setup="from __main__ import proc", number=3))
+    # [proc() for i in range(10)]
---
+++ append_to_array_with_loop.py
@@ -0,0 +1,25 @@
+from array import array
+from random import randint
+import sys
+
+
+def create_data():
+    a = array('i')
+    for i in range(100000):
+        a.append(randint(1, 10000000))
+    return a
+
+
+def proc():
+    cnt = 0
+    data = create_data()
+    for i in range(100000):
+        if randint(1, 10000000) in data:
+            cnt += 1
+
+if __name__ == '__main__':
+    print(sys.argv[0])
+    print(sys.version_info)
+    import timeit
+    print(timeit.timeit("proc()", setup="from __main__ import proc", number=3))
+    # [proc() for i in range(10)]
