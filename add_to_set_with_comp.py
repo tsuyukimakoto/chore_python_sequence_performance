7fd0a70d2c7ba389fad8253b44953b0865158f03
makoto tsuyuki <mtsuyuki@gmail.com>
2019-07-14 12:40:54 +0900

    for timeit

---
+++ add_to_set_with_comp.py
@@ -0,0 +1,21 @@
+from random import randint
+import sys
+
+
+def create_data():
+    return {randint(1, 10000000) for i in range(100000)}
+
+
+def proc():
+    cnt = 0
+    data = create_data()
+    for i in range(100000):
+        if randint(1, 10000000) in data:
+            cnt += 1
+
+if __name__ == '__main__':
+    print(sys.argv[0])
+    print(sys.version_info)
+    import timeit
+    print(timeit.timeit("proc()", setup="from __main__ import proc", number=1))
+    # [proc() for i in range(10)]
